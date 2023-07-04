48b5eda59cf9adaa3d527b73217ce45004e537de
Mr RoboT <robotmr969@gmail.com>
2023-07-04 22:20:44 +0300

    Deployed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ import "./Hero.css";
 const Hero = () => {
   return (
     <div className="Hero flex flex-col justify-center items-center pt-8 pb-6">
-      <img src={OmeaLogo} alt="OmeaLogo" className="w-[13rem] pb-2" />
+      <img src={OmeaLogo} alt="Omea Logo" className="w-[13rem] pb-2" />
       <img src={Text} alt="Omea Duties" />
     </div>
   );
